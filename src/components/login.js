44761aa09c6599eb5f396b3e63c7d65df9ce8e1a
saurav <sauravsingh90651@gmail.com>
2022-09-02 12:20:22 +0530

    initial

---
+++ src/components/login.js
@@ -0,0 +1,34 @@
+
+
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+export default function Login() {
+
+       const [name, setname] = useState("");
+       const [password, setpassword] = useState("");
+
+       const navigate = useNavigate();
+       const collectData = () => {
+              console.log(name, password);
+              setname(" ");
+              setpassword(" ");
+              navigate("/home");
+       }
+
+       return (<div>
+                      <ul className='nav-ul'>
+                             <h3>LogIn page</h3>
+                      </ul>
+                      <div className='login'>
+                             <h1> LogIn</h1>
+                             <input className='inputBox' type="text" placeholder='enter username'
+                                    value={name} onChange={(e) => setname(e.target.value)}
+                             />
+                             <input className='inputBox' type="password" placeholder='enter password'
+                                    value={password} onChange={(e) => setpassword(e.target.value)}
+                             />
+                             <button onClick={collectData} className='appButton' type='button'>LogIn</button>
+                      </div>
+               </div>
+       )
+}
